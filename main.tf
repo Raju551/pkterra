8aec95e7291792de5228ef70a7962e1150a4177d
Raju551 <86284611+Raju551@users.noreply.github.com>
2021-10-23 11:37:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,10 @@ resource "aws_instance" "app-server1" {
     Name = "pkdas-server-nilesh"
   }
 }
+output "myawsserver-ip" {
+  value = aws_instance.myawsserver.public_ip
+}
+
+output "myserver-PrivateIP" {
+  value = aws_instance.myawsserver.private_ip
+}
